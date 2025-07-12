a947f8dabd1c8e2ed638af734ee339d0f11b9355
0011223344ravi <22918656+0011223344ravi@users.noreply.github.com>
2025-07-12 14:50:46 +0530

    fc

---
+++ src/test/java/org/example/springboothelloapp/SpringbootHelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboothelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHelloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
